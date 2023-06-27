accab874a97f605e3a07743cf7a6010829fa3f13
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-06-27 20:21:42 +0200

    update

--- providers.tf
+++ providers.tf
@@ -21,9 +21,9 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_config_files      = ["/Users/dima/.aws/config"]
-  shared_credentials_files = ["/Users/dima/.aws/credentials"]
+  # region                   = "eu-central-1"
+  # shared_config_files      = ["/Users/dima/.aws/config"]
+  # shared_credentials_files = ["/Users/dima/.aws/credentials"]
 }
 
 provider "kubernetes" {
