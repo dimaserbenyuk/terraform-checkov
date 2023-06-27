dbd38124e82c0f90e783200fe301b2f12ca6d82e
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-06-27 22:07:26 +0200

    add eks

--- modules.tf
+++ modules.tf
@@ -58,3 +58,53 @@ module "s3_bucket" {
     Service = "s3"
   }
 }
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.14.0"
+  cluster_name    = "adonce-test"
+  cluster_version = "1.26"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 2
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "staging"
+  }
+
+}
