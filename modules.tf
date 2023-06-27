f7d4819fe9f782ab58568bf521deb5179fa25780
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-06-27 20:50:21 +0200

    add

--- modules.tf
+++ modules.tf
@@ -3,7 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "issue_vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.issue_vpc.id
+
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
