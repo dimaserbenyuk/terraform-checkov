fd6673ff172597fefc3433d8e40a5235e4e8a554
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-06-27 20:57:10 +0200

    add

--- modules.tf
+++ modules.tf
@@ -3,24 +3,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "issue_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.issue_vpc.id
-
-  ingress {
-    protocol  = "-1"
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
