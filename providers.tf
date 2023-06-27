e0992c4862f736e0a736f30d19c70cd29fc455be
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-06-27 20:23:57 +0200

    delete

--- providers.tf
+++ providers.tf
@@ -21,9 +21,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  # region                   = "eu-central-1"
-  # shared_config_files      = ["/Users/dima/.aws/config"]
-  # shared_credentials_files = ["/Users/dima/.aws/credentials"]
+
 }
 
 provider "kubernetes" {
